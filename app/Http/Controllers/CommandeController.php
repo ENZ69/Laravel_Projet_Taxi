5e86986dadd7fd6e724421e64d2242a5b4eabb4a
ENZ69 <scalerking72@gmail.com>
2023-03-30 22:51:38 +0000

    first commit

---
+++ app/Http/Controllers/CommandeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Commande;
+class CommandeController extends Controller
+{
+    public function store(Request $request){
+        $commande = new Commande();
+        $commande->trajet_id = $request->input('trajet_id');
+        $commande->passager_id = $request->input('passager_id');
+        $commande->save();
+
+        return view('succes');
+    }
+
+    public function index()
+    {
+        return view('homechauffeur', ['commandes' => Commande::with('passager', 'trajet')->get()]);
+    }
+}
---
+++ app/Models/Commande.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Trajet;
+
+class Commande extends Model
+{
+    use HasFactory;
+
+    public function trajet()
+    {
+        return $this->belongsTo(Trajet::class);
+    }
+
+    public function passager()
+    {
+        return $this->belongsTo(User::class, 'passager_id');
+    }
+
+    public function chauffeur()
+    {
+        return $this->belongsTo(User::class, 'chauffeur_id');
+    }
+
+}
